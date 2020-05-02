0cfdb92ea7b412dee12ab7b0a1b07c72de452579
eslam <eslamelkholy5325762@gmail.com>
2020-05-02 04:25:42 +0200

    Testing File

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -2,8 +2,14 @@ function studentController(studentSchema)
 {
     function post(request, response){
         const student=new studentSchema(request.body);
+        if(!request.body.Name)
+        {
+            response.status(400);
+            return response.send("Name is Required");
+        }
         student.save();
-        return response.status(201).json(student);
+        response.status(201);
+        return response.json(student);
     }
     function get(request, response){
         studentSchema.find({}).populate({path :"Courses Department"}).then((speakers) =>{
@@ -15,3 +21,11 @@ function studentController(studentSchema)
     return {post, get};
 }
 module.exports = studentController;
+
+// Short Describtion what this Controller Do
+/*
+    1- it Takes A Model Called StudentSchema
+    Which Were Injected In The Routers
+    2- Every Function Here takes the call Back Arguments
+    and Returns The Response
+*/
---
+++ tests/studentControllerTests.js
@@ -0,0 +1,28 @@
+const should = require("should");
+const sinon = require("sinon");
+const studentsController = require("../controllers/studentsController");
+
+describe("Book Controller Tests: ",() => {
+    describe("Post" ,() =>{
+        it("Should not allow to add Empty Name ", ()=>{
+            const studentSchema = function(student){
+                this.save = () => {};
+            }
+            const request = {
+                body : {
+                    EMail: "EslamElkholy@gmail.com"
+                }
+            }
+            const response = {
+                status: sinon.spy(),
+                send: sinon.spy(),
+                json: sinon.spy()
+            }
+            const controller = studentsController(studentSchema);
+            controller.post(request, response)
+            // Bad Request
+            response.status.calledWith(400).should.equal(true, `Bad Status: ${response.status.args[0][0]}`);
+            response.send.calledWith("Name is Required").should.equal(true);
+        })
+    });
+});
